d274fc6e2c560bc5e65a3bb6cf12c100325fcbae
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-19 21:13:36 -0400

    beginning of App building

--- src/index.js
+++ src/index.js
@@ -11,3 +11,8 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 //saving to github
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
